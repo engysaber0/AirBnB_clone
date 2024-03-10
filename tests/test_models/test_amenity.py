6b0d5c3ce51b1e68f0995a0d862dbc482ee62bcf
engysaber0 <engy8174@gmail.com>
2024-03-10 13:26:09 -0700

    AirBnB

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -90,14 +90,20 @@ class TestAmenity_instantiation(unittest.TestCase):
         """
         my_date = datetime.today()
         my_date_iso = my_date.isoformat()
-        amenity1 = Amenity(id="777", created_at=my_date_iso, updated_at=my_date_iso)
+        amenity1 = Amenity(
+            id="777",
+            created_at=my_date_iso,
+            updated_at=my_date_iso
+        )
         self.assertEqual(amenity1.id, "777")
         self.assertEqual(amenity1.created_at, my_date)
         self.assertEqual(amenity1.updated_at, my_date)
 
-    def test_instantiation_with_None_kwargs(self):
-        with self.assertRaises(TypeError):
-            Amenity(id=None, created_at=None, updated_at=None)
+
+def test_instantiation_with_None_kwargs(self):
+    with self.assertRaises(TypeError):
+        Amenity(id=None, created_at=None, updated_at=None)
+
 
 class TestAmenity_save(unittest.TestCase):
     """
